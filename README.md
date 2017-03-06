b562ebb2ba69bcd88736514d23d77551db2cdff9
Jacob Isbell <jacob.isbell@uiowa.edu>
2017-03-06 13:52:24 -0600

    Initial Population using Shea's documents.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# faraday_cnn
+# Faraday Complexity
+This project is an attempt to create a machine-learning algorithm (binary classifier) that will be able to tell whether a polarized radio spectrum is a simple "Faraday thin" source or a more complex source with multiple components or "Faraday thick". The classifier is a convolutional neural network (faraday_cnn.py), which uses the faraday spectrum as the input feature vector. This is all currently work-in-progress. The work is in collaboration with the POSSUM survey with the Australian Square Kilometre Array Pathfinder (ASKAP). 
+Bellow is an example of a simple Faraday thin source produced with create_spectrum.py
+
+![alt tag](https://github.com/sheabrown/faraday_complexity/blob/master/rm_spectrum.png)
+
+Here is a two component source, also created with create_spectrum.py, that has one component at 7 rad/m^2 and another at 40 rad/m^2. The frequency coverage mimics what will be available for POSSUM Early Science, though there could be an option to fill in the missing frequencies in some cases. 
+
+![alt tag](https://github.com/sheabrown/faraday_complexity/blob/master/QU_2.png)
+![alt tag](https://github.com/sheabrown/faraday_complexity/blob/master/Far_2.png)
---
+++ create_spectrum.py
@@ -0,0 +1,153 @@
+# =============================================================
+# create_spectrum.py contains functions for creating synthetic
+# polarization spectra over a range of frequencies. These 
+# spectra can be used to train an rm_machine to assess the
+# complexity of a Faraday spectrum for the POSSUM survey. 
+# =============================================================
+
+import numpy as np
+from matplotlib import pyplot as plt
+c=2.99e+08 #speed of light in m/s
+mhz=1.0e+06
+
+def createFrequency(numin=700.,numax=1800.,nchan=100.):
+	# Create an array of evenly spaced frequencies
+	# numin and numax are in [MHz]   
+	# ===========================================
+        numax=numax*mhz
+        numin=numin*mhz
+        nu=np.arange(nchan)*(numax-numin)/(nchan-1)+numin
+	return nu
+
+def createPOSSUMSpectrum():
+	# Create an array with POSSUM Early Science 
+	# frequency coverage
+	# ===========================================
+	band1=createFrequency(700.,1000.,nchan=300)
+	band2=createFrequency(1000.,1300.,nchan=300)
+	band3=createFrequency(1500.,1800.,nchan=300)
+	return np.concatenate((band1,band2,band3))
+	
+def createSpectrum(numin=700.,numax=1800.,nchan=100.,flux=1.,fdepth=7.,chinot=0.,freq=False):
+	try:
+		len(freq)
+		nu=freq
+	except TypeError:
+		nu=createFrequency(numin,numax,nchan)
+	P=flux*np.exp(2*1j*(chinot+fdepth*(c/nu)**2))
+	return nu, P
+
+def create2Spectrum(numin=700.,numax=1800.,nchan=100.,flux1=1.,flux2=1.,fdepth1=7.,fdepth2=70.,chinot1=0.,chinot2=0.,freq=False):
+        try:
+                len(freq)
+                nu=freq
+        except TypeError:
+                nu=createFrequency(numin,numax,nchan)
+        P=flux1*np.exp(2*1j*(chinot1+fdepth1*(c/nu)**2))+flux2*np.exp(2*1j*(chinot2+fdepth2*(c/nu)**2))
+        return nu, P
+
+def createNoiseSpectrum(numin=700.,numax=1800.,nchan=100.,sigma=0.01, freq=False):
+	try:
+		len(freq) 
+		nu=freq
+	except TypeError:
+		nu=createFrequency(numin,numax,nchan)
+        sig=sigma*np.random.standard_normal(nu.shape)
+        return nu, sig
+
+def createFaradaySpectrum(pol,nu,philow=-1000,phihi=1000):
+	F=[]
+	phi=[]
+	chinot=np.sqrt(np.mean((c/nu)**2))
+	for far in range(philow,phihi):
+		phi.append(far)
+		temp=np.sum(pol*np.exp(-2*1j*far*((c/nu)**2-chinot**2)))
+		F.append(temp)
+	return np.asarray(phi), np.asarray(F)/len(nu)
+
+
+def plot_pol(pol,nu):
+	fig, ax0 = plt.subplots(nrows=1)
+	ax0.errorbar(nu/1.0e+06,spec.real,yerr=np.ones(len(nu))*0.1,errorevery=10,markersize=4,label='Q',fmt='o')
+	ax0.errorbar(nu/1.0e+06,spec.imag,yerr=np.ones(len(nu))*0.1,errorevery=10,markersize=4,label='U',fmt='o')
+	ax0.set_title('Q & U')
+	ax0.set_xlabel(r'$\nu$ [MHz]')
+	ax0.set_ylabel(r'P($\nu$) [Jy/beam]')
+	ax0.set_xlim([700,1800])
+	#ax0.text(750, 0.85, r'$\phi=4 rad/m^2$')
+	legend = ax0.legend(loc='upper right', shadow=True)
+	plt.show()
+
+def plot_far(Faraday,phi):
+	fig, ax1 = plt.subplots(nrows=1)
+	ax1.errorbar(phi,np.abs(Faraday),yerr=np.ones(len(phi))*0.01,markersize=4,errorevery=10,fmt='o')
+	ax1.set_title('Faraday Spectrum')
+	ax1.set_xlabel(r'$\phi$ [rad/m$^2$]')
+	ax1.set_ylabel(r'F($\phi$) [Jy/beam]')
+	#ax1.text(55, 0.6, r'$\phi= $'+str(peak)+ r' $rad/m^2$')
+	#plt.subplots_adjust(hspace=0.5)
+	plt.show()
+
+def simulateData(n=10):
+	s=(n,200,2)
+	X=np.zeros(s)
+	Y=np.zeros(s[0])
+	for i in range(0,n/2-1):
+		nu, spec = createSpectrum(700.0,1800.0,100,1.0,i*10.0,0.0)
+		nu3, noise1 = createNoiseSpectrum(freq=nu,sigma=0.1)
+		spec += noise1+noise1*1j
+		phi, Faraday = createFaradaySpectrum(spec,nu, -100,100)
+		#plot_far(Faraday,phi)
+		X[i,:,0]=Faraday.imag
+		X[i,:,1]=Faraday.real
+		Y[i]=0
+
+	for i in range(n/2,n):
+        	nu, spec = create2Spectrum(700.0,1800.0,100,1.0,0.8,5.*i,10.*i,0.0,0.0)
+        	nu3, noise1 = createNoiseSpectrum(freq=nu,sigma=0.1)
+        	spec += noise1+noise1*1j
+        	phi, Faraday = createFaradaySpectrum(spec,nu, -100,100)
+        	#plot_far(Faraday,phi)
+        	X[i,:,0]=Faraday.imag
+        	X[i,:,1]=Faraday.real
+        	Y[i]=1
+
+	np.save('x_train.npy',X)
+	np.save('y_train.npy',Y)
+
+	for i in range(0,n/2-1):
+        	nu, spec = createSpectrum(700.0,1800.0,100,1.0,(i-6)*10.0,0.0)
+        	nu3, noise1 = createNoiseSpectrum(freq=nu,sigma=0.1)
+        	spec += noise1+noise1*1j
+        	phi, Faraday = createFaradaySpectrum(spec,nu, -100,100)
+        	X[i,:,0]=Faraday.imag
+        	X[i,:,1]=Faraday.real
+        	Y[i]=0
+
+	for i in range(n/2,n):
+        	nu, spec = create2Spectrum(700.0,1800.0,100,1.0,1.0,(i-4)*5.0,(i-3)*10.0,0.0,0.0)
+        	nu3, noise1 = createNoiseSpectrum(freq=nu,sigma=0.1)
+        	spec += noise1+noise1*1j
+        	phi, Faraday = createFaradaySpectrum(spec,nu, -100,100)
+        	X[i,:,0]=Faraday.imag
+        	X[i,:,1]=Faraday.real
+        	Y[i]=1
+
+	np.save('x_test.npy',X)
+	np.save('y_test.npy',Y)
+
+
+# Test some of the functions
+# ---------------------------
+nupos=createPOSSUMSpectrum()
+nu, spec = create2Spectrum(flux1=1.,flux2=1.,fdepth1=7.,fdepth2=40.,chinot1=0.,chinot2=0.,freq=nupos)
+nu3, noise1 = createNoiseSpectrum(freq=nu,sigma=0.1)
+phi, Faraday = createFaradaySpectrum(spec,nu)
+spec += noise1+noise1*1j
+#spec2 += noise1 + noise1*1j
+max=np.max(np.abs(Faraday))
+peak=phi[np.abs(Faraday)==max]
+print(peak)
+#plot_pol(spec,nu)
+#plot_far(Faraday,phi)
+simulateData(1000)
